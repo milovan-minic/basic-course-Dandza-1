a9f2f1eeb16ce37ebe5dbf14e6bd033556c0a8b7
Dandza <danijela.b.cuk@gmail.com>
2016-05-23 15:50:30 +0200

    Add files via upload

---
+++ task11.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 10;
+$y = 7;
+
+$c = $x + $y;
+echo "$x + $y = $c<br>";
+
+$c = $x - $y;
+echo "$x - $y = $c<br>";
+
+$c = $x * $y;
+echo "$x * $y = $c<br>";
+
+$c = $x / $y;
+echo "$x / $y = $c<br>";
+
+$c = $x % $y;
+echo "$x % $y = $c<br>";
+
+
+?>
